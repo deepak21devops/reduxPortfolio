e11f292303addaf84d4417e94372ccadbbad0db1
deepak <deepak21devops@gmail.com>
2021-10-09 08:32:56 +0530

    update bar final

--- src/Components/UpdateBar/UpdateBar.jsx
+++ src/Components/UpdateBar/UpdateBar.jsx
@@ -7,6 +7,26 @@ export default function UpdateBar() {
         <div className="updatebar">
             <div className="updateContainer">
                 <UpdateWar />
+                <div className="profileUpdate">
+                    <h3 className="heading">Profile Picture</h3>
+                    <div className="profileContent">
+                        <img className="profileimg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhyU6CVQ16FTCRV3u7AaQAiS53wwqfShSCkw&usqp=CAU" alt=""></img>
+                        <label for="file">Change</label>
+                        <input type="file" id="file" name="value" style={{ display: "none" }}></input>
+                    </div>
+                    <form className="formContent">
+                        <label>Username</label>
+                        <input type="text" placeholder="John"></input>
+
+                        <label>Email</label>
+                        <input type="text" placeholder="John123@gmail.com"></input>
+
+                        <label>Password</label>
+                        <input type="password"></input>
+
+                        <button className="updbtn">Update</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
